b1e85c355df954ac0c1056033a909e5ec45e172c
hasegawa218 <hasegawa2218rro@gmail.com>
2020-06-04 14:38:24 +0900

    slider animation 完成
    Signed-off-by: hasegawa218 <hasegawa2218rro@gmail.com>

--- templates/page-slider.php
+++ templates/page-slider.php
@@ -11,28 +11,28 @@ get_header(); ?>
 <!--content-->
 <section class="slideWrap js_wiper" style="position: relative;height:100vh;">
 <div class="slides">
-  <div class="top-slide_img_violet slide" style="width:100vw;height:100vh;position:absolute;z-index:0;">
-    <div class="aaa" style="position:relative;display:flex;">
-        <div style="position: absolute;" class="top-slide_img_item top-slide_img_item_1"></div>
+  <div class="top-slide_img_violet slide" style="width:100vw;height:100vh;position:absolute;">
+    <div class="aaa" style="position:relative;width:100%;height:100%;display:flex;">
+        <div class="top-slide_img_item top-slide_img_item_1"></div>
     </div>
   </div>
   <div class="top-slide_img_brown slide" style="width:100vw;height:100vh;position:absolute;z-index:0;">
-    <div class="aaa" style="position:relative;display:flex;">
-        <div style="display:none;position: absolute;" class="top-slide_img_item top-slide_img_item_2"></div>
+    <div class="aaa" style="position:relative;width:100%;height:100%;display:flex;">
+        <div class="top-slide_img_item top-slide_img_item_2"></div>
     </div>
   </div>
   <div class="top-slide_img_brown slide" style="width:100vw;height:100vh;position:absolute;z-index:0;">
-    <div class="aaa" style="position:relative;display:flex;">
-        <div style="display:none;position: absolute;" class="top-slide_img_item top-slide_img_item_3"></div>
+    <div class="aaa" style="position:relative;width:100%;height:100%;display:flex;">
+        <div class="top-slide_img_item top-slide_img_item_3"></div>
     </div>
   </div>
 
-  <div class="top-slider_btn">
-    <div class="top-slider_btn_left slider-btn prevBtn"><i class="fas fa-chevron-left"></i></div>
-    <div class="top-slider_btn_right slider-btn nextBtn"><i class="fas fa-chevron-right"></i></div>
+  <div class="top-slide_btn">
+    <div class="top-slide_btn_left slide-btn prevBtn"><i class="fas fa-chevron-left"></i></div>
+    <div class="top-slide_btn_right slide-btn nextBtn"><i class="fas fa-chevron-right"></i></div>
   </div>
 </div>
-<div class="top-slider_text" style="position:relative;display:flex;z-index:100;flex-direction:column;">
+<div class="top-slide_text">
     <h2>Made with<br>TEINEI</h2>
     <p>ていねいな人のために、<br>ていねいにつくりました。</p>
     <div>OUR MESSAGE</div>
